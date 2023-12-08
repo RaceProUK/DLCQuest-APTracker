73808e3853ac95604286ff079def9cdf6147a52f
RaceProUK <raceprouk@hotmail.co.uk>
2023-12-08 18:36:48 +0000

    Added AP Reset event handler

--- pack/scripts/archipelago.lua
+++ pack/scripts/archipelago.lua
@@ -34,26 +34,28 @@ function Reset(slotData)
     end
     if slotData["campaign"] then
         local setting = Tracker:FindObjectForCode("Campaign")
-        setting.CurrentStage = slotData["campaign"]
+        setting.CurrentStage = tonumber(slotData["campaign"])
     end
     if slotData["ending_choice"] then
         local setting = Tracker:FindObjectForCode("Ending")
-        setting.Active = slotData["ending_choice"]
+        setting.Active = slotData["ending_choice"] ~= 0
     end
     if slotData["coinsanity"] then
         local setting = Tracker:FindObjectForCode("CoinBundleSize")
         if slotData["coinbundlerange"] then
-            setting.AcquiredCount = slotData["coinbundlerange"]
+            setting.AcquiredCount = tonumber(slotData["coinbundlerange"])
         else
             setting.AcquiredCount = 0
         end
     end
     if slotData["item_shuffle"] then
         local setting = Tracker:FindObjectForCode("ItemsShuffled")
-        setting.Active = slotData["item_shuffle"]
+        setting.Active = slotData["item_shuffle"] ~= 0
     end
     if slotData["death_link"] then
         local setting = Tracker:FindObjectForCode("DeathLink")
-        setting.Active = slotData["death_link"]
+        setting.Active = slotData["death_link"] ~= 0
     end
 end
+
+Archipelago:AddClearHandler("Reset", Reset)
