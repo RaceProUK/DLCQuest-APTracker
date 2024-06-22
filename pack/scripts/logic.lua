de9c7f81bd58a411d90bdebd44a0a023b7b20279
RaceProUK <raceprouk@hotmail.co.uk>
2024-06-22 23:26:06 +0100

    LFOD: Updated logic for all checks east of Name Change Pack

--- pack/scripts/logic.lua
+++ pack/scripts/logic.lua
@@ -1,4 +1,4 @@
-function HarmlessPlantsPackAccessible()
+function CanPassNameChangeGate()
     local hpp = Tracker:FindObjectForCode("PackHarmlessPlants")
     local ncp = Tracker:FindObjectForCode("PackNameChange")
     return hpp and ncp and (ncp.Active or not hpp.Active)
