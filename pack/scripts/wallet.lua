f54eef51f382fe4c121f4995cd2b9c7789f58fa8
RaceProUK <raceprouk@hotmail.co.uk>
2024-08-10 13:54:36 +0100

    Removing more debug code

--- pack/scripts/wallet.lua
+++ pack/scripts/wallet.lua
@@ -10,20 +10,16 @@ end
 
 function Wallet:DepositDLCQ(amount)
     Wallet.DLCQBalance = Wallet.DLCQBalance + tonumber(amount)
-    print("DLCQ Deposit " .. amount .. ", new balance " .. Wallet.DLCQBalance)
 end
 
 function Wallet:DepositLFOD(amount)
     Wallet.LFODBalance = Wallet.LFODBalance + tonumber(amount)
-    print("LFOD Deposit " .. amount .. ", new balance " .. Wallet.LFODBalance)
 end
 
 function Wallet:WithdrawDLCQ(amount)
     Wallet.DLCQBalance = Wallet.DLCQBalance - tonumber(amount)
-    print("DLCQ Withdrawal " .. amount .. ", new balance " .. Wallet.DLCQBalance)
 end
 
 function Wallet:WithdrawLFOD(amount)
     Wallet.LFODBalance = Wallet.LFODBalance - tonumber(amount)
-    print("LFOD Withdrawal " .. amount .. ", new balance " .. Wallet.LFODBalance)
 end
